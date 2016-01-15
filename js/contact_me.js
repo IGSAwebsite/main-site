12e2c6fc2dbe7ab6323d44c6e1023b718b7b62db
Aditya Purandare <aditya.p1993@hotmail.com>
2016-01-15 16:48:02 -0600

    Check for HTTP verb

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././contact_us.php",
+                url: "contact_us.php",
                 type: "POST",
                 data: {
                     name: name,
