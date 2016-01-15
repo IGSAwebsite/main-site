5142e43593d04949415338a48b43aa00d681f313
Alok Satpathy <aloks1990@gmail.com>
2016-01-14 18:23:01 -0600

    Contact mail edit

--- contact_us.php
+++ contact_us.php
@@ -6,15 +6,22 @@ $subject = $_POST['subject'];
 $message = $_POST['message'];
 
 echo $name.$emailid.$subject.$message;
+//
+// $to      = 'aloks1990@gmail.com';
+// $subject = $subject;
+// $message = "Hello from ".$name." <".$emailid.">".$message;
+// $headers = 'From: aloks1990@gmail.com'."\r\n".'Reply-To: aloks1990@gmail.com'."\r\n".'X-Mailer: PHP/'.phpversion();
+//
+// mail($to, $subject, $message, $headers);
 
 $to      = 'aloks1990@gmail.com';
-$subject = $subject;
-$message = "Hello from ".$name." <".$emailid."> \n".$message;
-$headers = 'From: aloks1990@gmail.com' . "\r\n" .
-    'Reply-To: aloks1990@gmail.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: aloks1990@gmail.com" . "\r\n" .
+"CC: aditya.p1993@hotmail.com";
 
-mail($to, $subject, $message, $headers);
+mail($to,$subject,$txt,$headers);
+
 
 echo "Email sent and here the email:".$message;
 
