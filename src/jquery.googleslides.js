f8e391ed1eb376faf3508cbe1059dc2dfe2c4fa8
Aditya Purandare <aditya.p1993@hotmail.com>
2016-01-14 20:15:04 -0600

    Remove Https

--- src/jquery.googleslides.js
+++ src/jquery.googleslides.js
@@ -15,12 +15,12 @@
 			  'time'	 		 : 5000,
 			  'fadespeed'		 : 1000
 	};
-			
+
 	var methods = {
 		init: function (options) {
 		    var settings = $.extend({}, defaults, options);
 			this.data('googleslidesOptions', settings);
-			
+
 			if ($('.googleslides[albumid=' + settings.albumid +']').length > 0) {
 				var error = 'jQuery.googleslides ERROR: albumid:' + settings.albumid + ' is already on the page.  Only one album per page is supported.';
 				this.text(error);
@@ -34,16 +34,16 @@
                   authKeyStr = '&authkey=' + settings.authkey;
                 }
 
-				var albumJsonUrl = '<script src="https://picasaweb.google.com/data/feed/base/user/' + settings.userid + '/albumid/' + settings.albumid 
-					+ '?alt=json&kind=photo&max-results=' + settings.maxresults + '&hl=en_US&imgmax=' + settings.imgmax  
+				var albumJsonUrl = '<script src="http://picasaweb.google.com/data/feed/base/user/' + settings.userid + '/albumid/' + settings.albumid 
+					+ '?alt=json&kind=photo&max-results=' + settings.maxresults + '&hl=en_US&imgmax=' + settings.imgmax
 					+ authKeyStr
-					+ '&callback=jQuery.fn.googleslides.prepare_' + settings.albumid + '&fields=link,entry(link,media:group(media:content,media:description))">' 
+					+ '&callback=jQuery.fn.googleslides.prepare_' + settings.albumid + '&fields=link,entry(link,media:group(media:content,media:description))">'
 					+ '</sc' + 'ript>';
-				
-				var prepareFunCallback = 'jQuery.fn.googleslides.prepare_' + settings.albumid 
+
+				var prepareFunCallback = 'jQuery.fn.googleslides.prepare_' + settings.albumid
 					+ ' = function(data) { $(".googleslides[albumid=' + settings.albumid + ']").googleslides("prepare", data); };';
 				eval(prepareFunCallback);
-				
+
 				this.width(settings.imgmax);
 				this.addClass('googleslides');
 				$('body').append(albumJsonUrl);
@@ -67,32 +67,32 @@
 					slide.append($('<a target="_blank" href="' + link + '"></a>'));
 					slideInner = slide.children().first();
 				}
-				
+
 				slideInner.append($('<img src="' + url + '" alt="' + caption + '"/>'));
 
 				$("img", slideInner).width(width).height(height);
-				
+
 				if (settings.caption == true && caption != '') {
 					slideInner.append('<div class="captionWrapper"><div class="caption">' + caption + '</div></div>');
 					$(".captionWrapper", slideInner).width(settings.imgmax);
 				}
-				
+
 				slides.push(slide);
 			}
-			
+
 			if (settings.random == true) {
 				slides.sort(methods.randomSort);
 			}
-			
+
 			for (var i = 0; i < slides.length; i++) {
 				this.append(slides[i]);
 			}
-			
+
 			//set height/width of container so that it is just big enough to contain all the images
 			this.height(Math.max.apply(Math, $('.googleslide img', this).map(function(){ return $(this).height(); }).get()) + 2);
-			
+
 			this.width(Math.max.apply(Math, $('.googleslide img', this).map(function(){ return $(this).width(); }).get()) + 2);
-			
+
 			this.googleslides('start');
 		},
 		randomSort: function (a, b) {
@@ -100,9 +100,9 @@
 		},
 		start: function () {
 			var settings = this.data('googleslidesOptions');
-			
-			this.find('.googleslide').first().fadeIn(settings.fadespeed);	
-			
+
+			this.find('.googleslide').first().fadeIn(settings.fadespeed);
+
 			var target = this;
 			setInterval(function() {
 				var first = target.find('.googleslide').first();
@@ -118,7 +118,7 @@
 		}
 	}
 
-  $.fn.googleslides = function(method) {  
+  $.fn.googleslides = function(method) {
 		 // Method calling logic
 		if ( methods[method] ) {
 		  return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
@@ -126,6 +126,6 @@
 		  return methods.init.apply( this, arguments );
 		} else {
 		  $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
-		}    		
+		}
   };
 })( jQuery );
