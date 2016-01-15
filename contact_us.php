1aa48a551cd23de38e04c6e685c63d25d198b572
Alok Satpathy <aloks1990@gmail.com>
2016-01-14 18:38:18 -0600

    Add HTML Email

--- contact_us.php
+++ contact_us.php
@@ -16,12 +16,13 @@ echo $name.$emailid.$subject.$message;
 
 $to      = 'aloks1990@gmail.com';
 $subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: aloks1990@gmail.com" . "\r\n" .
-"CC: aditya.p1993@hotmail.com";
+$txt = "<html><p>Hello world!</p></html>";
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0"."\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8"."\r\n";
+$headers .= "From: aloks1990@gmail.com"."\r\n"."CC: aditya.p1993@hotmail.com";
 
 mail($to,$subject,$txt,$headers);
-
 
 echo "Email sent and here the email:".$message;
 
