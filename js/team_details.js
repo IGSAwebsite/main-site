0bac7166c66f0a7fd6127bb5483400fec19c5ae1
mathangiK <mathangi1993@gmail.com>
2016-10-16 13:56:24 -0500

    Team Page Update

---
+++ js/team_details.js
@@ -0,0 +1,259 @@
+//JavaScript for team_details.html
+
+/*
+* contains the team information
+*/
+var team_details = {
+	IT: {
+		team_count : 5,
+		team_name : 'Information Technology',
+		teamMember1:{
+			name : 'Yeshwanth JA',
+			picture_path: 'img/team_images/it_yashwanth_1.jpg'
+		},
+		teamMember2:{
+			name : 'Prasanna Vignesh Narayana',
+			picture_path: 'img/team_images/it_prasanna_2.jpg'
+		},	
+		teamMember3:{
+			name : 'Naman Jadhav',
+			picture_path: 'img/team_images/it_naman_3.jpg'
+		},
+		teamMember4:{
+			name : 'Mathangi Krishnamurthi',
+			picture_path: 'img/team_images/it_math_4.jpg'
+		},
+		teamMember5:{
+			name : 'Shyam Sundar Esanaka Harikumar',
+			picture_path: 'img/team_images/it_shyam_5.jpg'
+		}
+	},	
+	advocacy: {
+		team_count : 3,
+		team_name : 'Advocacy',
+		teamMember1:{
+			name : 'Aishwarya Wadhwani',
+			picture_path: 'img/team_images/advocacy_aishwarya_1.jpg'
+		},
+		teamMember2:{
+			name : 'Terin Thomas',
+			picture_path: 'img/team_images/advocacy_terin_2.jpg'
+		},	
+		teamMember3:{
+			name : 'Ajinkya	Ranadive',
+			picture_path: 'img/team_images/advocacy_ajinkya_3.jpg'
+		}
+	},
+	editorial: {
+		team_count : 4,
+		team_name : 'Editorial',
+		teamMember1:{
+			name : 'Aalhad Parulekar',
+			picture_path: 'img/team_images/editorial_aalhad_1.jpg'
+		},
+		teamMember2:{
+			name : 'Ramprasath	Babu',
+			picture_path: 'img/team_images/editorial_ramprasath_2.jpg'
+		},	
+		teamMember3:{
+			name : 'Ishan Khurjekar',
+			picture_path: 'img/team_images/editorial_ishan_3.jpg'
+		},
+		teamMember4:{
+			name : 'NFN	Gompa Pranathi',
+			picture_path: 'img/team_images/editorial_pranathi_4.jpg'
+		}
+	},
+	events: {
+		team_count : 8,
+		team_name : 'Events Planning',
+		teamMember1:{
+			name : 'Akash Bajaj',
+			picture_path: 'img/team_images/event_akash_1.jpg'
+		},
+		teamMember2:{
+			name : 'Satish Kaladagimath',
+			picture_path: 'img/team_images/event_satish_2.jpg'
+		},	
+		teamMember3:{
+			name : 'Sudhanshu Kumar	Singh',
+			picture_path: 'img/team_images/event_sudanshu_3.jpg'
+		},
+		teamMember4:{
+			name : 'Asok Mani Sidharth',
+			picture_path: 'img/team_images/event_asok_4.jpg'
+		},
+		teamMember5:{
+			name : 'Nupur Jain',
+			picture_path: 'img/team_images/event_nupur_5.jpg'
+		},
+		teamMember6:{
+			name : 'Vamsi Immadi',
+			picture_path: 'img/team_images/event_vamsi_6.jpeg'
+		},	
+		teamMember7:{
+			name : 'Archana	Adarsh Rao',
+			picture_path: 'img/team_images/event_archana_7.jpg'
+		},
+		teamMember8:{
+			name : 'Shrikkanth	Ramesh',
+			picture_path: 'img/team_images/event_shrikkanth_8.jpg'
+		}
+	}, 
+	finance: {
+		team_count : 4,
+		team_name : 'Finance',
+		teamMember1:{
+			name : 'Rahul Sharma',
+			picture_path: 'img/team_images/finance_rahul_1.jpg'
+		},
+		teamMember2:{
+			name : 'Sumay Bhojwani',
+			picture_path: 'img/team_images/finance_sumay_2.jpg'
+		},	
+		teamMember3:{
+			name : 'Deepa Rao',
+			picture_path: 'img/team_images/finance_deepa_3.jpg'
+		},
+		teamMember4:{
+			name : 'Komal Koya',
+			picture_path: 'img/team_images/finance_komal_4.jpg'
+		}
+	},
+	mentoring: {
+		team_count : 4,
+		team_name : 'Mentoring',
+		teamMember1:{
+			name : 'Shoa Khan',
+			picture_path: 'img/team_images/mentoring_shoa_1.jpg'
+		},
+		teamMember2:{
+			name : 'Animesh	Mathur',
+			picture_path: 'img/team_images/mentoring_animesh_2.jpg'
+		},	
+		teamMember3:{
+			name : 'Abhiram	Dinavahi',
+			picture_path: 'img/team_images/mentoring_abhiram_3.jpg'
+		},
+		teamMember4:{
+			name : 'Mohit Sharma',
+			picture_path: 'img/team_images/mentoring_mohit_4.jpg'
+		}
+	},
+	marketing: {
+		team_count : 4,
+		team_name : 'Marketing',
+		teamMember1:{
+			name : 'Afdal Basheer',
+			picture_path: 'img/team_images/marketing_afdal_1.jpg'
+		},
+		teamMember2:{
+			name : 'Priyanka Vikramadithyan',
+			picture_path: 'img/team_images/marketing_priyanka_2.jpg'
+		},	
+		teamMember3:{
+			name : 'Adithya	M K',
+			picture_path: 'img/team_images/marketing_adithya_3.jpg'
+		},
+		teamMember4:{
+			name : 'Monisha	Padur',
+			picture_path: 'img/team_images/marketing_monisha_4.jpg'
+		}
+	},
+	networking: {
+		team_count : 5,
+		team_name : 'Networking',
+		teamMember1:{
+			name : 'Hemik Parikh',
+			picture_path: 'img/team_images/network_hemik_1.jpg'
+		},
+		teamMember2:{
+			name : 'Visakh Nair',
+			picture_path: 'img/team_images/network_visakh_2.jpg'
+		},	
+		teamMember3:{
+			name : 'Kriti Verma',
+			picture_path: 'img/team_images/network_kriti_3.jpg'
+		},
+		teamMember4:{
+			name : 'Sashank	Babdhakavi',
+			picture_path: 'img/team_images/network_sashank_4.jpg'
+		},
+		teamMember5:{
+			name : 'Kranti	Garatkar',
+			picture_path: 'img/team_images/network_kranti_5.jpg'
+		}
+	},
+	pr: {
+		team_count : 4,
+		team_name : 'Public Relations',
+		teamMember1:{
+			name : 'Aditi Kothari',
+			picture_path: 'img/team_images/pr_aditi_1.jpg'
+		},
+		teamMember2:{
+			name : 'Mandar Nevpurkar',
+			picture_path: 'img/team_images/pr_mandar_2.jpg'
+		},	
+		teamMember3:{
+			name : 'Rituparna Das',
+			picture_path: 'img/team_images/pr_ritu_3.jpg'
+		},
+		teamMember4:{
+			name : 'Talib Ahmad',
+			picture_path: 'img/team_images/pr_talib_4.jpg'
+		},
+		teamMember5:{
+			name : 'A. Pawan Dixit',
+			picture_path: 'img/team_images/pr_pawan_5.jpg'
+		}
+	},
+	
+}
+
+/*onReady function called as soon as the jQuery is loaded */
+$( document ).ready(function() {
+    console.log( "ready!" );
+	console.log(decodeURIComponent($.urlParam('teamName')));
+	generateHTMLTeamDetails(decodeURIComponent($.urlParam('teamName')));
+});
+
+/*
+* Creates the dynamic html content based on team infromation
+*/
+function generateHTMLTeamDetails(teamName){
+	var htmlCodeToInject = '';
+	$('#team_title').html('<span>'+team_details[teamName]['team_name']+' Team</span>');
+	var team_count = parseInt(team_details[teamName]['team_count']);
+	var count_to_three = 1;
+	for(var j=1;j<=team_count;j++){
+		var member_name = team_details[teamName]['teamMember'+j]['name'];
+		var member_image_location = team_details[teamName]['teamMember'+j]['picture_path'];
+		if(count_to_three == 1)
+			htmlCodeToInject += '<ul class="polaroids row" >';
+		htmlCodeToInject += '<li class="col-sm-4" style="padding-top:40px;">'+
+			'<a href="www.google.com" title="'+member_name+'">'+
+				'<img src="'+member_image_location+' ">'+
+			'</a>'+
+		'</li>';
+		if(count_to_three == 3){
+			htmlCodeToInject += '</ul>';
+			count_to_three = 0;
+		}
+		count_to_three = count_to_three + 1;
+	}
+	$('#polaroid_canvas').html(htmlCodeToInject);
+}
+
+/*
+* Parsing parameter from URL
+* In this case need to read the URL for the team name and according display team
+*/
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    } else{
+       return results[1] || 0;
+    }
+}	
